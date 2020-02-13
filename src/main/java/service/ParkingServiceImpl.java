bc328492f234e8ca70870a5de5b66b0dac284d56
niyazkadirov <niyaz.kadirov@yandex.ru>
2020-02-13 11:33:59 +0300

    fixed bug №24

--- src/main/java/service/ParkingServiceImpl.java
+++ src/main/java/service/ParkingServiceImpl.java
@@ -24,20 +24,16 @@ public class ParkingServiceImpl implements ParkingService {
         while (true) {
             String s = scanner.nextLine();
             if (s.isEmpty()) {
-
-
                 int randomNumber;
                 if (getParkingSize() <= 5) {
                     randomNumber = 1;
                 } else {
                     int i = getParkingSize() / 3;
                     randomNumber = random.nextInt(i); //nextInt от 1 до n (+1 делает включительно)
-                    while (randomNumber == 0){
+                    while (randomNumber == 0) {
                         randomNumber = random.nextInt(i); //nextInt от 1 до n (+1 делает включительно)
                     }
                 }
-
-
                 if (isEmptyParking()) {
 
                     for (int i = 1; i <= randomNumber; i++) {
@@ -45,19 +41,7 @@ public class ParkingServiceImpl implements ParkingService {
                             carList.add(new Car(random.nextInt(100)));
                         }
                     }
-
-//                    System.out.println("Number of parking spaces: " + ((getParkingSize() - carList.size()) - 1));
-
-                    emptySeat = randomNumber - (getParkingSize() - carList.size());
-
-//                    carList.stream().distinct().forEach(car -> System.out.println("Iteration before leaving the parking lot : " + car.getParkingExpired() +
-//                            "  Parking place: " + carList.indexOf(car)));
-
-
                 }
-
-                // System.out.println("Number of parking spaces: " + ((getParkingSize() - carList.size())));
-
                 for (Car car : carList) {
                     car.setParkingExpired(car.getParkingExpired() - 1);
                 }
@@ -65,6 +49,7 @@ public class ParkingServiceImpl implements ParkingService {
                 carList.removeIf(car -> car.getParkingExpired() <= 0);
 
                 System.out.println("Number of parking spaces: " + ((getParkingSize() - carList.size())));
+                emptySeat = randomNumber - (getParkingSize() - carList.size());
 
 
                 for (int i = 1; i <= emptySeat; i++) {
@@ -73,7 +58,7 @@ public class ParkingServiceImpl implements ParkingService {
 
                 List<Integer> collect = carList.stream().map(Car::getParkingExpired).collect(Collectors.toList());
                 if (!collect.isEmpty()) {
-                        System.out.println("Parking will be empty in " + Collections.min(collect) + " later iteration");
+                    System.out.println("Parking will be empty in " + Collections.min(collect) + " later iteration");
                 }
                 carList.stream().distinct().forEach(car -> System.out.println("Iteration before leaving the parking lot : " + car.getParkingExpired() +
                         "  Parking place: " + carList.indexOf(car)));
