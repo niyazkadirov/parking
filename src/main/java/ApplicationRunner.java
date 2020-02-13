9e207de7f86202680d06b71352990810218adfca
niyazkadirov <niyaz.kadirov@yandex.ru>
2020-02-13 18:21:57 +0300

    Interface segregation

--- src/main/java/ApplicationRunner.java
+++ src/main/java/ApplicationRunner.java
@@ -1,12 +1,12 @@
-import model.Parking;
-import service.ParkingService;
+import service.ParkingServiceImpl;
+
 import static service.PrinterService.printQuestionToConsole;
 
 public class ApplicationRunner {
     public static void main(String[] args) {
-        ParkingService parkingService = new ParkingService();
+        ParkingServiceImpl parkingServiceImpl = new ParkingServiceImpl();
 
-        printQuestionToConsole(parkingService);
+        printQuestionToConsole(parkingServiceImpl);
     }
 }
 
