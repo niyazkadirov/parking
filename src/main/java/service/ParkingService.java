e4950c81f9504befab8ea7d6288bfef2a7e81584
niyazkadirov <niyaz.kadirov@yandex.ru>
2020-02-13 17:06:24 +0300

    refactor

--- src/main/java/service/ParkingService.java
+++ src/main/java/service/ParkingService.java
@@ -1,8 +1,97 @@
 package service;
 
-public interface ParkingService {
+import model.Car;
+import model.Parking;
 
-    boolean isEmptyParking();
+import java.util.*;
 
-    void addCarsToList() throws InterruptedException;
+import static service.PrinterService.*;
+
+
+public class ParkingService {
+    private List<Car> carList = new ArrayList<>();
+    private Random random = new Random(System.currentTimeMillis());
+    private Parking parking = new Parking();
+    private Scanner scanner = new Scanner(System.in);
+
+
+
+    public boolean isFreePlace() {
+        return carList.size() < parking.getParkingSize();
+    }
+
+
+
+
+    public void addCarsToList() {
+
+        while (true) {
+            String s = scanner.nextLine();
+            if (!s.isEmpty()) {
+                continue;
+            }
+
+            int randomNumber = getRandomNumber(parking);
+
+            if (isFreePlace()) {
+                for (int i = 1; i <= randomNumber; i++) {
+
+                    if (!isFreePlace()) {
+                        continue;
+                    }
+                    carList.add(new Car(random.nextInt(10)));
+                }
+            }
+
+
+            decrementCarList(carList);
+
+            carList.removeIf(car -> car.getRemainingIterate() <= 0);
+
+            printNumberParkingSpace(parking, carList);
+
+            int emptyPlace = randomNumber - (parking.getParkingSize() - carList.size());
+
+
+            printNotPlaceInParking(emptyPlace);
+
+            printParkingPlaceInfo(carList);
+        }
+    }
+
+    public int getParkingSizeFromConsole(){
+        Scanner parkingSize = new Scanner(System.in);
+        int parkingSizeFromConsole = parkingSize.nextInt();
+        parking.setParkingSize(parkingSizeFromConsole);
+        return parkingSizeFromConsole;
+    }
+
+    public void decrementCarList(List<Car> carList){
+        for (Car car : carList) {
+            car.setRemainingIterate(car.getRemainingIterate() - 1);
+        }
+    }
+
+    public int getRandomNumber(Parking parking){
+        int randomNumber;
+        if (parking.getParkingSize() <= 5) {
+            randomNumber = 1;
+        } else {
+            int i = parking.getParkingSize() / 3;
+            randomNumber = random.nextInt(i);
+        }
+        return randomNumber;
+    }
+
+    public void generateCarsForList1(List<Class>T, int randomNumber){
+        if (isFreePlace()) {
+            for (int i = 1; i <= randomNumber; i++) {
+
+                if (!isFreePlace()) {
+                    continue;
+                }
+                carList.add(new Car(random.nextInt(10)));
+            }
+        }
+    }
 }
--- src/main/java/service/PrinterService.java
+++ src/main/java/service/PrinterService.java
@@ -1,6 +1,53 @@
 package service;
 
-public interface PrinterService {
+import model.Car;
+import model.Parking;
 
-    void printQuestionToConsole(ParkingServiceImpl parkingService);
+import java.util.InputMismatchException;
+import java.util.List;
+
+public class PrinterService {
+
+
+    public static void printQuestionToConsole(ParkingService parkingService) {
+
+        System.out.println("Enter parking size:");
+        int parkingSizeFromConsole = parkingService.getParkingSizeFromConsole();
+
+        try {
+            if (parkingSizeFromConsole <= 0) {
+                System.out.println("Invalid input, please try again");
+                printQuestionToConsole(parkingService);
+            }
+            parkingService.addCarsToList();
+        } catch (InputMismatchException e) {
+            System.out.println("Invalid input, please try again");
+            printQuestionToConsole(parkingService);
+        }
+    }
+
+
+    static void printNumberParkingSpace(Parking parking, List<Car> carList) {
+        System.out.println("Number of parking spaces: " + ((parking.getParkingSize() - carList.size())));
+    }
+
+    static void printNotPlaceInParking(int emptyPlace) {
+        for (int i = 1; i <= emptyPlace; i++) {
+            System.out.println("Sorry, we don't have any openings.");
+        }
+    }
+
+    static void printParkingPlaceInfo(List<Car> carList) {
+        int remainingIterate = Integer.MAX_VALUE;
+
+        for (Car car : carList) {
+            System.out.println("Iteration before leaving the parking lot : " + car.getRemainingIterate() + "  Parking place: " + carList.indexOf(car));
+            if (remainingIterate > car.getRemainingIterate()) {
+                remainingIterate = car.getRemainingIterate();
+            }
+        }
+        System.out.println("Parking will be empty in " + remainingIterate + " later iteration");
+    }
+
+
 }
