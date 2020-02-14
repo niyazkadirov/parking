13aeaa69ca179041016e759eb1ce71d8b177984c
Niyaz Kadirov <niyaz.kadirov@yandex.ru>
2020-02-14 18:53:04 +0300

    Added feature: Commands for the command line [status, help, clear all, clear <index>]
    Implemented verification of the validity of commands.

--- src/main/java/service/ParkingServiceImpl.java
+++ src/main/java/service/ParkingServiceImpl.java
@@ -12,41 +12,66 @@ import static service.PrinterService.*;
 
 
 public class ParkingServiceImpl implements ParkingService {
-    private static final int UPPER_RANGE_RANDOM = 10;
+    private static final int UPPER_RANGE_RANDOM = 50;
     private List<Car> carList = new ArrayList<>();
     private Random random = new Random(System.currentTimeMillis());
     private Parking parking = new Parking();
     private Scanner scanner = new Scanner(System.in);
-
 
     @Override
     public boolean isHasFreePlace() {
         return parking.getParkingSize() > carList.size();
     }
 
-
     @Override
     public void parkingHandler() {
 
+
         while (true) {
-            String s = scanner.nextLine();
-            if (!s.isEmpty()) {
-                continue;
+
+
+            String command = scanner.nextLine();
+            switch (command) {
+                case ("status"):
+                    printParkingPlaceInfo(carList);
+                    continue;
+
+                case ("help"):
+                 printHelpCommands();
+                    continue;
+
+                case (""):
+                    emptyMoveHandler();
+                    continue;
+
+                case ("clear all"):
+                    clearAll(carList);
+                    continue;
+
+                default:
+                    if (!clearByIndex(command)) {
+                        System.out.println("Invalid command");
+                    }
             }
+        }
+    }
 
-            int randomNumber = getRandomNumber(parking);
-            decrementAndRemoveCarList(carList);
-            generateCarsAndAddToList(carList, randomNumber);
-            printNumberParkingSpace(parking, carList);
+    @Override
+    public void emptyMoveHandler() {
+        int randomNumber = getRandomNumber(parking);
+        decrementAndRemoveCarList(carList);
+        generateCarsAndAddToList(carList, randomNumber);
 
+        if (printAndGetNumberParkingSpace(parking, carList) == 0) {
+            printIterBeforeLeavingParking(carList);
             int emptyPlace = randomNumber - (parking.getParkingSize() - carList.size());
             printNotPlaceInParking(emptyPlace);
-            printParkingPlaceInfo(carList);
         }
     }
 
     @Override
     public int getParkingSizeFromConsole() {
+
         int parkingSizeFromConsole = scanner.nextInt();
         parking.setParkingSize(parkingSizeFromConsole);
         return parkingSizeFromConsole;
@@ -85,5 +110,37 @@ public class ParkingServiceImpl implements ParkingService {
                 carList.add(new Car(remainingIterate <= 0 ? 1 : remainingIterate));
             }
         }
+    }
+
+    @Override
+    public void clearAll(List<Car> carList) {
+        carList.removeAll(carList);
+    }
+
+    @Override
+    public boolean clearByIndex(String command) {
+        String[] arrCommand = command.split(" ");
+        if (arrCommand[0].equals("clear")) {
+            try {
+                carList.remove(Integer.parseInt(arrCommand[1]));
+                return true;
+            } catch (IndexOutOfBoundsException | NumberFormatException ignored) {
+                System.out.println("Элемент не найден");
+            }
+        }
+        return false;
+    }
+
+    public void printHelpCommands(){
+        System.out.println(
+                " -------------------------------------------------------------------------------- \n" +
+                "| <status> - Displays all current cars with their lifetime and parking number.   |\n" +
+                "| <clear all> - completely cleans parking.                                       |\n" +
+                "| <clear [index]> - cleans parking by id                                         |\n" +
+                " --------------------------------------------------------------------------------"
+
+
+        );
+
     }
 }
